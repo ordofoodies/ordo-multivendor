66cc1573ae8a9316e684e0864d3b725e3f230a5b
faizan tayyab <faizantayyab37@gmail.com>
2025-11-20 16:42:47 +0500

    updated sentry keys

--- ordo-multivendor-rider/environment.ts
+++ ordo-multivendor-rider/environment.ts
@@ -16,7 +16,7 @@ const getEnvVars = (env = Updates.channel) => {
       WS_GRAPHQL_URL: "wss://aws-server.enatega.com/graphql",
       SENTRY_DSN:
         configuration?.riderAppSentryUrl ??
-        "https://e963731ba0f84e5d823a2bbe2968ea4d@o1103026.ingest.sentry.io/6135261",
+        "https://797ccc3f12e20a2abb9966e2a798c31d@o4510397051502592.ingest.de.sentry.io/4510397125361744",
       // GOOGLE_MAPS_KEY: 'AIzaSyBk4tvTtPaSEAVSvaao2yISz4m8Q-BeE1M',
       GOOGLE_MAPS_KEY:configuration?.googleApiKey,
       ENVIRONMENT: "production",
